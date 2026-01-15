6cf6f22a0bd58b9bb1b803190f7e4f402dc5dbb1
rcopra <137560494+rcopra@users.noreply.github.com>
2026-01-15 12:42:52 +0100

    Markdown preview won't close

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -5,4 +5,7 @@ return {
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = { 'markdown' },
   build = ':call mkdp#util#install()',
+  init = function()
+    vim.g.mkdp_auto_close = 0 -- Auto-close when leaving buffer
+  end,
 }
