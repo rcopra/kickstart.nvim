e38bd270b9c83de8ddadcb63ad90c2cdee716b7f
rcopra <137560494+rcopra@users.noreply.github.com>
2026-01-19 10:51:39 +0100

    Pg up and Down

--- init.lua
+++ init.lua
@@ -208,6 +208,8 @@ vim.keymap.set('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper wi
 -- Scroll half-page and center cursor
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
+vim.keymap.set('n', '<PageDown>', '<C-d>zz', { desc = 'Scroll down and center' })
+vim.keymap.set('n', '<PageUp>', '<C-u>zz', { desc = 'Scroll up and center' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
