9aa63d89118b6275ab228ecebeb9be5fafcc636a
Rick <137560494+rcopra@users.noreply.github.com>
2026-02-13 20:24:17 -0300

    add java

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -211,6 +211,7 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        jdtls = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
