987a8ac4595de2956b27b85b2ea75ba0d8ea5f8f
rcopra <137560494+rcopra@users.noreply.github.com>
2026-03-26 20:43:04 +0100

    highlight colors plugin

---
+++ lua/custom/plugins/highlight-colors.lua
@@ -0,0 +1,9 @@
+return {
+  'brenoprata10/nvim-highlight-colors',
+  event = 'VeryLazy',
+  opts = {
+    render = 'virtual', -- 'background' | 'foreground' | 'virtual'
+    enable_named_colors = true,
+    enable_tailwind = false,
+  },
+}
