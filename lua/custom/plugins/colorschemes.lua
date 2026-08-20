e4f5cacde66c5ec03bd98a33c8c9b1ce574a27cd
rcopra <137560494+rcopra@users.noreply.github.com>
2026-08-20 22:47:27 +0200

    yes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -3,69 +3,13 @@
 vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
 
 require('catppuccin').setup {
-  flavour = 'macchiato', -- latte, frappe, macchiato, mocha
-  transparent_background = false,
+  flavour = 'auto', -- latte, frappe, macchiato, mocha
+  background = {
+    light = 'latte',
+    dark = 'mocha',
+  },
   show_end_of_buffer = false,
-  term_colors = false,
-  dim_inactive = {
-    enabled = false,
-    shade = 'dark',
-    percentage = 0.15,
-  },
-  no_italic = false,
-  no_bold = false,
-  no_underline = false,
-  styles = {
-    comments = { 'italic' },
-    conditionals = { 'italic' },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = {},
-    operators = {},
-  },
-  color_overrides = {},
-  custom_highlights = {},
-  default_integrations = true,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    treesitter = true,
-    notify = false,
-    mini = {
-      enabled = true,
-      indentscope_color = '',
-    },
-    telescope = { enabled = true },
-    which_key = true,
-    mason = true,
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = { 'italic' },
-        hints = { 'italic' },
-        warnings = { 'italic' },
-        information = { 'italic' },
-        ok = { 'italic' },
-      },
-      underlines = {
-        errors = { 'underline' },
-        hints = { 'underline' },
-        warnings = { 'underline' },
-        information = { 'underline' },
-        ok = { 'underline' },
-      },
-      inlay_hints = {
-        background = true,
-      },
-    },
-  },
+  auto_integrations = true,
 }
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme 'catppuccin-nvim'
